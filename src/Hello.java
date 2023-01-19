42af33defab73ba67eb2811e4b4255af8f7ff385
Zheng Qiao <qz940342897@gmail.com>
2023-01-19 14:04:58 -0800

    Hello World

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Joe");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
